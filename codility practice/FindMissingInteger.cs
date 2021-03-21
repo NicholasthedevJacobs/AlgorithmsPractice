74907e4ae010f0f112841a9df318db2cbfe363da
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2021-03-21 14:31:10 -0500

    Init

---
+++ codility practice/FindMissingInteger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codility_practice
+{
+    class FindMissingInteger
+    {
+        public static int solution(int[] A)
+        {
+            HashSet<int> hash = new HashSet<int>(A);
+            var noDuplicates = hash;
+
+            int[] cleanArray = new int[noDuplicates.Count];
+
+            noDuplicates.CopyTo(cleanArray);
+
+            Array.Sort(cleanArray);
+            var found = 1;
+            for (int i = 0; i < cleanArray.Length; i++)
+            {
+                if (cleanArray[i] <= 0)
+                {
+                    continue;
+
+                }
+                else if (cleanArray[i] == found)
+                {
+                    found++;
+                }
+
+            }
+
+            return found;
+
+        }
+    }
+}
---
+++ codility practice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace codility_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] A = new int[] { 5, 1, 2, 4, 5 };
+            //var findMissingInteger = new FindMissingInteger();
+            //var solution = FindMissingInteger.solution(A);
+            //Console.WriteLine(solution);
+            
+        }
+
+        
+
+       
+        
+    }
+}
