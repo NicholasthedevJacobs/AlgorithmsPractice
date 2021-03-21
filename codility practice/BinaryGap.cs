505fe39d98b9156c48c15f8a781479a77de7bbf7
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2021-03-21 14:40:27 -0500

    Convert int to binary

--- codility practice/BinaryGap.cs
+++ codility practice/BinaryGap.cs
@@ -6,5 +6,13 @@ namespace codility_practice
 {
     class BinaryGap
     {
+        public int solution(int N)
+        {
+            string binary = Convert.ToString(N, 2);
+
+            Console.WriteLine(binary);
+
+            return 1;
+        }
     }
 }
