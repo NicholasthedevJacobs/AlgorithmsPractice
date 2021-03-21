4bf207147c0a2cef028adebdcc1fdefea8b6dd47
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2021-03-21 15:27:38 -0500

    First iteration of solution.

--- codility practice/BinaryGap.cs
+++ codility practice/BinaryGap.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace codility_practice
@@ -8,11 +9,37 @@ namespace codility_practice
     {
         public int solution(int N)
         {
+            N = 9898;
             string binary = Convert.ToString(N, 2);
+            var split = binary.ToCharArray();
 
-            Console.WriteLine(binary);
+            var counter = 0;
+            bool foundOne = false;
+            List<int> counterList = new List<int>();
 
-            return 1;
+            for(int i = 0; i < split.Length; i++)
+            {
+                if (split[0] == '1' && i == 0)
+                {
+                    foundOne = true;
+                }
+                else if (split[i] == '0')
+                {
+                    counter++;
+                    if(i == split.Length - 1)
+                    {
+                        counterList.Add(counter);
+                    }
+                }
+                else if (split[i] == '1' && split[i - 1] == '0')
+                {
+                    counterList.Add(counter);
+                    counter = 0;
+                }             
+            }
+
+            int solution = counterList.Max();
+            return solution;
         }
     }
 }
